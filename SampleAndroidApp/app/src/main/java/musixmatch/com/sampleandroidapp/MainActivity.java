cad1e4672f13e71e24f61691e54f695452444e34
Loreto Parisi <loretoparisi@gmail.com>
2016-01-13 11:50:40 +0100

    Added sample Android app using Silver shared library

---
+++ SampleAndroidApp/app/src/main/java/musixmatch/com/sampleandroidapp/MainActivity.java
@@ -0,0 +1,99 @@
+package musixmatch.com.sampleandroidapp;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.os.AsyncTask;
+
+// Shared Library Test
+import com.remobjects.elements.system.Action1;
+
+import samplelibrary.*;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        // Async Task Shared Library Test
+        BrowserTask task = new BrowserTask();
+        task.execute();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private class BrowserTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params) {
+
+            SharedClassTest sampleAPI = new SharedClassTest();
+            String apiURL = "https://api.spotify.com/v1/search?q=tania%20bowra&type=artist";
+
+            Log.d("TEST", "Now calling sdk");
+
+            sampleAPI.httpCall__completion(apiURL, new Action1<String>() {
+                @Override
+                public void run(String s) {
+                    Log.d("TEST", s);
+                }
+            });
+
+            sampleAPI.databaseSetup();
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+        }
+
+        @Override
+        protected void onPreExecute() {
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+        }
+    }
+}
