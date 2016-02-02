aee497389f5f2e1798e66337f2c172a61e84e37e
Loreto Parisi <loretoparisi@gmail.com>
2016-02-02 17:15:49 +0100

    Fixed Action closure in .NET ConsoleApplication example

--- NetConsoleApplication/NetConsoleApplication/Program.cs
+++ NetConsoleApplication/NetConsoleApplication/Program.cs
@@ -16,17 +16,20 @@ namespace NetConsoleApplication
 			await Task.Delay(10);
 
 			SharedClassTest sampleAPI = new SharedClassTest ();
-			String apiURL = "https://api.spotify.com/v1/search?q=tania%20bowra&type=artist";
-
-
 			sampleAPI.setup();
 
-			sampleAPI.getJsonObject__success__error(apiURL, (result) => {
-				Console.WriteLine("RESPONSE", result);
-			},
-			(e) => {
-				Console.WriteLine("EXCEPTION", e.Message);
-			});
+			String apiURL = "https://api.spotify.com/v1/search?q=tania%20bowra&type=artist";
+
+			Action<String> success = (String value) => {
+				Console.WriteLine("RESPONSE");
+				Console.WriteLine(value);
+			};
+
+			Action<Exception> error = (Exception exception) => {
+				Console.WriteLine("Error");
+				Console.WriteLine(exception);
+			};
+			sampleAPI.getJsonObject__success__error(apiURL, success, error);
 
 			Console.WriteLine();
 		}
