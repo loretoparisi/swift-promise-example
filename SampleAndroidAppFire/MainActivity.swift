fac92a3498ae6ff69c13ae060b4505779f1c8527
Loreto Parisi <loretoparisi@gmail.com>
2016-03-04 14:48:44 +0100

    improvements in Android Silver app

--- SampleAndroidAppFire/MainActivity.swift
+++ SampleAndroidAppFire/MainActivity.swift
@@ -11,7 +11,21 @@ import sugar
 public class MainActivity: Activity {
 	
 	// The Shared Library Core
-	var sampleAPI:SharedClassTest?;
+	var sampleAPI:SharedClassTest!;
+	
+	private func testAPIGetJson() -> () {
+		let url = "https://api.spotify.com/v1/search?q=tania%20bowra&type=artist";
+			
+		let successAction:Action1<sugar.String?> = { result in
+			Log.d("TEST", result);
+		};
+		let errorAction:Action1<Exception?> = { error in
+			if let err = error {
+				Log.d("TEST", err.toString());
+			}
+		};
+		sampleAPI.getJsonString(url, success:successAction, error: errorAction);
+	}
 
 	public override func onCreate(savedInstanceState: Bundle!) {
 		super.onCreate(savedInstanceState)
@@ -19,55 +33,19 @@ public class MainActivity: Activity {
 		
 		Log.d("TEST", "Hello Android on Fire!");
 		
-		// Calling Application class (see application tag in AndroidManifest.xml)
-		
-		// Shared Library instance
-
-		if let self.sampleAPI = SharedClassTest() {
-			// set application context
-			sampleAPI.context(getApplicationContext());
-
-			// database setup
-			sampleAPI.setup();
-			
-			let url = "https://api.spotify.com/v1/search?q=tania%20bowra&type=artist";
-			
-			let successAction:Action1<sugar.String?> = { result in
-				Log.d("TEST", result);
-			};
-			let errorAction:Action1<Exception?> = { error in
-				if let err = error {
-					Log.d("TEST", err.toString());
-				}
-			};
-			
-			/*let successDelegate = {
-				// [self]   // Original version had this not commented; it's commented to let people know it was a typo
-				class SomethingClass {
-					func run(s:String) -> () {  Log.d("TEST", s); }
-				}
-				return SomethingClass()
-			}()
-			
-			let errorDelegate = {
-				// [self]   // Original version had this not commented; it's commented to let people know it was a typo
-				class SomethingClass {
-					func run(e:Exception) -> () {  Log.d("TEST", e.toString()); }
-				}
-				return SomethingClass()
-			}()*/
-			
-			sampleAPI.getJsonString(url, success:successAction, error: errorAction);
-		}
+		self.sampleAPI = SharedClassTest();
+		// set application context
+		self.sampleAPI.context(getApplicationContext());
+		// database setup
+		self.sampleAPI.setup();
+	
 	}
 	
 	public override func onResume() {
 		super.onResume();
-
-		// set application context
-		/*if let sampleAPI = self.sampleAPI {
-			sampleAPI.context(getApplicationContext());
-		}*/
+		
+		self.sampleAPI.context(getApplicationContext());
+		self.testAPIGetJson();
 
 	}
 }
